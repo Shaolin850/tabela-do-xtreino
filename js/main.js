cb39af4c777fa8bd5211f631676021b375af2311
Shaolin850 <120032684+Shaolin850@users.noreply.github.com>
2025-09-16 19:29:50 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.querySelector('form');
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const username = document.querySelector('#username').value;
-        alert(`Welcome, ${username}!`);
-    });
-});
